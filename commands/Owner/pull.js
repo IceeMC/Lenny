082dcb6adbce332da068ca1649bd527c8b791a23
IceeMC <johnburke915@gmail.com>
2018-12-19 17:09:15 -0800

    fix pull.js

--- commands/Owner/pull.js
+++ commands/Owner/pull.js
@@ -17,7 +17,7 @@ class Pull extends Command {
         const output = stdout ? `**\`Updated\`**${this.client.utils.codeBlock(stdout, "prolog")}` : "";
 		const err = stderr ? `**\`Error while updating\`**${this.client.utils.codeBlock(stderr, "prolog")}` : "";
         await message.send([output, err].join("\n"));
-        process.exit();
+        if (stdout !== "Already up-to-date") process.exit();
 	}
 
 };
