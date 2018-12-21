27bdb9ebcac2ab77528a07b29b0329e947a9bb26
IceeMC <johnburke915@gmail.com>
2018-12-20 20:58:41 -0800

    ficx

--- commands/Economy/daily.js
+++ commands/Economy/daily.js
@@ -13,9 +13,10 @@ class Daily extends Command {
     async run(message) {
         if (Date.now() < message.member.config.lastDaily)
             throw message.language.get("COMMAND_DAILY_ALREADY_CLAIMED", this.client.utils.formatMS(message.member.config.lastDaily));
-        await message.member.setCoins(2500);
+        const coins = (await this.client.utils.isVoter(message.author.id)) ? 3500 : 2500;
+        await message.member.setCoins(coins);
         await message.member.updateConfig({ lastDaily: Date.now() + 864e5 });
-        return message.sendLocale("COMMAND_DAILY_CLAIMED", [2500, (await this.client.utils.isVoter(message.author.id))]);
+        return message.sendLocale("COMMAND_DAILY_CLAIMED", [coins, (await this.client.utils.isVoter(message.author.id))]);
     }
 
 }
