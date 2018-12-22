88520ebcf2d74198e1ab098f1bde7a7220219e2c
Ice <31800435+IceeMC@users.noreply.github.com>
2018-12-21 19:00:55 -0800

    fix member arg

--- args/member.js
+++ args/member.js
@@ -13,7 +13,7 @@ class MemberArg extends Arg {
         console.log(arg);
         const [id] = idRgx.test(arg) ? idRgx.exec(arg) : [null];
         const [tag] = tagRgx.test(arg) ? tagRgx.exec(arg) : [null];
-        const [mention] = mentionRgx.test(arg) ? mentionRgx.exec(arg) : [null];
+        const [, mention] = mentionRgx.test(arg) ? mentionRgx.exec(arg) : [null, null];
         if (id) {
             const member = message.guild.members.get(id);
             if (!member) throw message.language.get("ARG_BAD_MEMBER");
@@ -24,10 +24,11 @@ class MemberArg extends Arg {
             return member;
         } else if (mention) {
             return message.mentions.members.get(mention);
+        } else {
+            const member = message.guild.members.find(m => m.user.username.toLowerCase() === arg.toLowerCase());
+            if (!member) throw message.language.get("ARG_BAD_MEMBER");
+            return member;
         }
-        const member = message.guild.members.find(m => m.user.username.toLowerCase() === arg.toLowerCase());
-        if (!member) throw message.language.get("ARG_BAD_MEMBER");
-        return member;
     }
 
 }
