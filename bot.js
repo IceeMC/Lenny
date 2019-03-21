e2092d64fa793a736091828b4737edce6508cb0d
Ice <31800435+IceeMC@users.noreply.github.com>
2019-03-20 22:21:57 -0700

    bye sentry

--- bot.js
+++ bot.js
@@ -1,14 +1,8 @@
 require("./framework/extendedStructures.js");
 const ChatNoirClient = require("./framework/ChatNoirClient.js");
-const Raven = require("raven");
 const client = new ChatNoirClient({
     disableEveryone: true,
     disabledEvents: ["TYPING_START", "GUILD_SYNC", "RELATIONSHIP_ADD", "RELATIONSHIP_REMOVE", "USER_SETTINGS_UPDATE", "USER_NOTE_UPDATE"],
     reconnect: true
 });
-
-Raven.config(client.config.sentry, {
-    captureUnhandledRejections: true
-}).install();
-
-Raven.context(() => client.login());
+client.login();
