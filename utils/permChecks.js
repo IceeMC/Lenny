51dc8a9ed68782a23b36b4ba98546d8e8ae67993
Ice <31800435+IceeMC@users.noreply.github.com>
2019-03-21 20:45:46 -0700

    Update permChecks.js

--- utils/permChecks.js
+++ utils/permChecks.js
@@ -4,20 +4,20 @@ const constants = require("./Constants.js");
 module.exports = new PermChecks()
     // Anyone can use
     .add(0, () => true)
-    // Beta testers
-    .add(2, (client, message) => {
-        const guild = client.guilds.get(constants.guild);
-        const role = guild ? guild.roles.get(constants.roles.beta) : null;
-        if (!guild || !role) return false;
-        return client.config.developers.includes(message.author.id) || message.member.roles.has(role.id);
-    })
-    // Premium members
-    .add(3, (client, message) => {
-        const guild = client.guilds.get(constants.guild);
-        const role = guild ? guild.roles.get(constants.roles.premium) : null;
-        if (!guild || !role) return false;
-        return client.config.developers.includes(message.author.id) || message.member.roles.has(role.id);
-    })
+//     // Beta testers
+//     .add(2, (client, message) => {
+//         const guild = client.guilds.get(constants.guild);
+//         const role = guild ? guild.roles.get(constants.roles.beta) : null;
+//         if (!guild || !role) return false;
+//         return client.config.developers.includes(message.author.id) || message.member.roles.has(role.id);
+//     })
+//     // Premium members
+//     .add(3, (client, message) => {
+//         const guild = client.guilds.get(constants.guild);
+//         const role = guild ? guild.roles.get(constants.roles.premium) : null;
+//         if (!guild || !role) return false;
+//         return client.config.developers.includes(message.author.id) || message.member.roles.has(role.id);
+//     })
     // Guild Managers
     .add(5, (_, message) => message.guild && message.member.permissions.has("MANAGE_GUILD"))
     // Moderator
