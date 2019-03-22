acf0664446975c96dd2265812b39346a063f0b07
Ice <31800435+IceeMC@users.noreply.github.com>
2019-03-21 20:47:33 -0700

    Fix constants

--- utils/Constants.js
+++ utils/Constants.js
@@ -1,12 +1,12 @@
 const { readFileSync } = require("fs");
 
 module.exports = {
-    guild: "397479560876261377",
-    joinsChannel: "454776806869041154",
-    upvoteChannel: "501826341205835787",
-    bugsChannel: "460800229667504148",
-    errorsChannel: "454776836929617921",
-    suggestionsChannel: "460801007769616394",
-    roles: { beta: "506119917024575509", premium: "476467857543266316" },
+    guild: "558035191193665537",
+    joinsChannel: "558421538358034432",
+    upvoteChannel: "558496638646091816",
+    bugsChannel: "558496765507272725",
+    errorsChannel: "558421478408978442",
+    suggestionsChannel: "558496898525167617",
+    // roles: { beta: "506119917024575509", premium: "476467857543266316" },
     tttBoard: readFileSync(`${process.cwd()}/assets/ttt.png`)
 };
