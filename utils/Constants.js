bea0f6b1338d4808e0980848e3bf1e7f9f643706
Ice <31800435+IceeMC@users.noreply.github.com>
2019-04-17 13:22:15 -0700

    Yay

--- utils/Constants.js
+++ utils/Constants.js
@@ -2,11 +2,11 @@ const { readFileSync } = require("fs");
 
 module.exports = {
     guild: "558035191193665537",
-    joinsChannel: "558421538358034432",
-    upvoteChannel: "558496638646091816",
-    bugsChannel: "558496765507272725",
-    errorsChannel: "558421478408978442",
-    suggestionsChannel: "558496898525167617",
+    joinsChannel: "568167373618806814",
+    upvoteChannel: "568167420737355980",
+    bugsChannel: "568167475582337035",
+    errorsChannel: "568167528430567424",
+    suggestionsChannel: "568167576593629185",
     // roles: { beta: "506119917024575509", premium: "476467857543266316" },
     tttBoard: readFileSync(`${process.cwd()}/assets/ttt.png`)
 };
